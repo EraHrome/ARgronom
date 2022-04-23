c4fea2313b950a99189c45e8d6a8ed498ee516ef
Nikita <polkovnikovnikita99@yandex.ru>
2022-04-23 09:28:26 +0300

    основная frontend - тема

--- ARgronom/ARgronom/Models/Role.cs
+++ ARgronom/ARgronom/Models/Role.cs
@@ -4,25 +4,14 @@ namespace ARgronom.Models
 {
     public enum Role
     {
-
-        /// <summary>
-        /// Пользователь
-        /// </summary>
+        [Description("Неавторизованный пользователь")]
+        None,
         [Description("Пользователь")]
-        User,
-        /// <summary>
-        /// Продавец
-        /// </summary>
+        Customer,
         [Description("Продавец")]
         Seller,
-        /// <summary>
-        /// Модератор
-        /// </summary>
         [Description("Модератор")]
         Moderator,
-        /// <summary>
-        /// Администратор
-        /// </summary>
         [Description("Администратор")]
         Admin
 
