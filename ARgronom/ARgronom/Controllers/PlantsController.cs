b101372d7d4bad1c0aeb2776a8fed9a720b5bb17
Ziamrurth <93211239@mail.ru>
2022-04-23 15:35:50 +0300

    Добавлена детальная карточка продукта

---
+++ ARgronom/ARgronom/Controllers/PlantsController.cs
@@ -0,0 +1,30 @@
+﻿using ARgronom.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARgronom.Controllers
+{
+    public class PlantsController : Controller
+    {
+        private readonly Context _context;
+
+        public PlantsController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var plant = _context.Plants.FirstOrDefault(p => p.Id == id);
+            return View(plant);
+        }
+    }
+}
