43652c32cac1f889642d98a48ed1d0517f4c84b7
Nikita <polkovnikovnikita99@yandex.ru>
2022-04-23 20:46:26 +0300

    добавлена поддержка AR

--- ARgronom/ARgronom/Controllers/ArController.cs
+++ ARgronom/ARgronom/Controllers/ArController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
 using ARgronom.Contexts;
+using System.Linq;
+using ARgronom.Models;
+using System.Security.Claims;
+using ARgronom.Models.ViewModels;
 
 namespace ARgronom.Controllers
 {
@@ -15,9 +19,40 @@ namespace ARgronom.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string plantId)
         {
-            return View();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userMarkers = _context.Markers.Where(x => x.UserId == userId).ToList();
+            var model = new ArViewModel()
+            {
+                Markers = userMarkers,
+                PlantId = plantId
+            };
+            return View(model);
+        }
+
+        public IActionResult AddCoord(string latit, string longit, string plantId)
+        {
+            var plant = _context.UserPlants.First(x => x.PlantId == plantId);
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var oldMarker = _context.Markers.FirstOrDefault(x => x.PlantId == plantId && x.UserId == userId);
+            if (oldMarker != null)
+            {
+                _context.Markers.Remove(oldMarker);
+                _context.SaveChanges();
+            }
+
+            var newMarker = new Marker()
+            {
+                Latitude = latit,
+                Longitude = longit,
+                PlantId = plantId,
+                UserId = userId
+            };
+            _context.Markers.Add(newMarker);
+            _context.SaveChanges();
+            return RedirectToAction("MyDetail", "PersonalArea", new { plantId = plantId });
         }
 
     }
---
+++ ARgronom/ARgronom/Models/ViewModels/ArViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ARgronom.Models.ViewModels
+{
+    public class ArViewModel
+    {
+        public string PlantId { get; set; }
+        public List<Marker> Markers { get; set; }
+    }
+}
